496f82f032555c579e1c516c3b1b688e6203a389
Daniil Baturin <daniil@baturin.org>
2021-07-31 23:25:20 +0700

    Move the remaining functions out of the Graph module and reformat the code a bit.

--- src/lib/tsort.ml
+++ src/lib/tsort.ml
@@ -148,33 +148,28 @@ let sort nodes =
   | [] -> Sorted sorted_node_ids
   | _  -> ErrorCycle remaining_ids
 
-(*
-   Deal with cyclic graphs.
-*)
-module Graph = struct
-  type ('a, 'b) t = ('a, 'b list) Hashtbl.t
+(* Functions for dealing with cyclic graphs. *)
 
-  let transpose tbl =
-    let tbl2 = Hashtbl.create 100 in
-    let init v =
-      if not (Hashtbl.mem tbl2 v) then
-        Hashtbl.add tbl2 v []
-    in
-    Hashtbl.iter (fun u vl ->
-      init u;
-      List.iter (fun v ->
-        let ul =
-          try Hashtbl.find tbl2 v
-          with Not_found -> []
-        in
-        Hashtbl.replace tbl2 v (u :: ul)
-      ) vl
-    ) tbl;
-    tbl2
+let transpose tbl =
+  let tbl2 = Hashtbl.create 100 in
+  let init v =
+    if not (Hashtbl.mem tbl2 v) then
+      Hashtbl.add tbl2 v []
+  in
+  Hashtbl.iter (fun u vl ->
+    init u;
+    List.iter (fun v ->
+      let ul =
+	try Hashtbl.find tbl2 v
+	with Not_found -> []
+      in
+      Hashtbl.replace tbl2 v (u :: ul)
+    ) vl
+  ) tbl;
+  tbl2
 
-  let _to_list tbl =
-    Hashtbl.fold (fun u vl acc -> (u, vl) :: acc) tbl []
-end
+let _to_list tbl =
+  Hashtbl.fold (fun u vl acc -> (u, vl) :: acc) tbl []
 
 (*
    Sort the results of 'partition' so as to follow the original node
@@ -182,7 +177,7 @@ end
 *)
 let sort_partition graph_l clusters =
   let priority = Hashtbl.create 100 in
-  List.iteri (fun i (v, _) -> Hashtbl.replace priority v i) graph_l;
+  let () = List.iteri (fun i (v, _) -> Hashtbl.replace priority v i) graph_l in
   let prio v =
     try Hashtbl.find priority v
     with Not_found -> assert false
@@ -194,8 +189,7 @@ let sort_partition graph_l clusters =
   in
   let cmp u v = compare (prio u) (prio v) in
   let cmp_list ul vl = compare (list_prio ul) (list_prio vl) in
-  List.map (fun l -> List.sort cmp l) clusters
-  |> List.sort cmp_list
+  List.map (fun l -> List.sort cmp l) clusters |> List.sort cmp_list
 
 (*
    Implementation of Kosaraju's algorithm for partitioning a graph into its
@@ -204,7 +198,7 @@ let sort_partition graph_l clusters =
 let partition graph_l =
   let graph = graph_hash_of_list graph_l in
   let graph_l = add_missing_nodes graph_l graph in
-  let tr_graph = Graph.transpose graph in
+  let tr_graph = transpose graph in
   let visits = Hashtbl.create 100 in
   let is_visited v = Hashtbl.mem visits v in
   let mark_visited v = Hashtbl.replace visits v () in
@@ -217,48 +211,40 @@ let partition graph_l =
     with Not_found -> assert false
   in
   let rec visit acc v =
-    if not (is_visited v) then (
-      mark_visited v;
-      let out_neighbors = get_out_neighbors v in
-      let acc =
-        List.fold_left (fun acc u -> visit acc u) acc out_neighbors in
-      v :: acc
-    )
-    else
-      acc
+    if not (is_visited v) then
+      begin
+        mark_visited v;
+        let out_neighbors = get_out_neighbors v in
+        let acc = List.fold_left (fun acc u -> visit acc u) acc out_neighbors in
+        v :: acc
+      end
+    else acc
   in
   let l =
-    List.fold_left (fun acc (v, _vl) ->
-      visit acc v
-    ) [] graph_l
+    List.fold_left (fun acc (v, _vl) -> visit acc v) [] graph_l
   in
   let assignments = Hashtbl.create 100 in
   let is_assigned v = Hashtbl.mem assignments v in
   let rec assign v root =
-    if not (is_assigned v) then (
+    if not (is_assigned v) then begin
       Hashtbl.replace assignments v root;
       let in_neighbors = get_in_neighbors v in
-      List.iter (fun u ->
-        assign u root
-      ) in_neighbors
-    )
+      List.iter (fun u -> assign u root) in_neighbors
+    end
   in
-  List.iter (fun v ->
-    assign v v
-  ) l;
+  let () = List.iter (fun v -> assign v v) l
   (* end Kosaraju's algorithm *)
-
+  in
   let clusters = Hashtbl.create 100 in
-  Hashtbl.iter (fun v id ->
+  let () = Hashtbl.iter (fun v id ->
     let members =
       try Hashtbl.find clusters id
       with Not_found -> []
     in
     Hashtbl.replace clusters id (v :: members)
-  ) assignments;
-  let partition =
-    Hashtbl.fold (fun _id members acc -> members :: acc) clusters []
+  ) assignments
   in
+  let partition = Hashtbl.fold (fun _id members acc -> members :: acc) clusters [] in
   graph_l, sort_partition graph_l partition
 
 let find_strongly_connected_components graph_l =
